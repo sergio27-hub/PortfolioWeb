23a4a377c8824fe90c8f2389a99389aa1ec2edcb
sergio <120045754+sergio27-hub@users.noreply.github.com>
2024-03-22 09:03:05 +0100

    update new modification in home for delay

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,12 +37,12 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
     })
 
 /*SCROLL HOME*/
-sr.reveal('.home_title', {})
-sr.reveal('.button', { delay: 200 })
-sr.reveal('.home_social-icon', { interval: 200 })
+sr.reveal('.home_title', { delay: 200 , interval: 300})
+sr.reveal('.button', { delay: 400 })
+sr.reveal('.home_social-icon', { interval: 400 })
 /*SCROLL ABOUT*/
 sr.reveal('.about_img', { delay: 400 })
-sr.reveal('.about_subtitle', { delay: 200 })
+sr.reveal('.about_subtitle', { delay: 500 })
 sr.reveal('.about_profession', { delay: 300 })
 sr.reveal('.about_text', { delay: 400 })
 sr.reveal('.about_social-icon', { delay: 500 })
